310ca7b36306573cf9b985a2ced21821a81c1709
Zidan Omar Hamid <hzidanomar@gmail.com>
2022-08-26 12:01:17 +0300

    add docker compose file and set testing connection

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 
-const db = require('./database/models');
+const db = require('./models');
 
 require('dotenv').config();
 
 const app = express();
 
-db.sequelize.sync({ force: false }).then(() => {
-  console.log('Connected to DB');
-
-  const __PORT__ = process.env.PORT || 5000;
-  app.listen({ port: __PORT__ }, async () => {
-    console.log(`Server started on port ${__PORT__}`);
-  });
+/**
+ * * Testing path
+ */
+app.get('/', (_, res) => {
+  res.status(200).json({ data: 'hello world!' });
 });
+
+// db.sequelize.sync({ force: false }).then(() => {
+//   console.log('Connected to DB');
+
+const __PORT__ = process.env.PORT || 5000;
+app.listen({ port: __PORT__ }, async () => {
+  console.log(`Server started on port ${__PORT__}`);
+});
+// });
